884b71a52f9e8df259c38cf56c4bc90a9ead305c
Cyril Beeckman <cyril.beeckman@gmail.com>
2023-02-26 14:31:45 +0100

    Clean this shit

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,5 +2,5 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 "yes"
 
-shared_scripts { "@es_extended/imports.lua", "config.lua" }
-server_scripts { "server.lua" }
+shared_scripts { "@es_extended/imports.lua" }
+server_scripts { "config.lua", "server.lua" }
