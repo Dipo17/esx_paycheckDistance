6c8157551338b6d3df137fde7d00438e2a07b0be
Dipo <107956809+Dipo17@users.noreply.github.com>
2024-05-06 22:22:04 +0200

    Code cleaned up

--- server.lua
+++ server.lua
@@ -1,26 +1,37 @@
+-- Note: This awesome aribo addict guy insulted me when I posted my first script :)
+
+local interval = Config.Interval*60000
+
 CreateThread(function()
 	while true do
-		local xPlayers = ESX.GetPlayers()
-
-		for i = 1, #xPlayers, 1 do
-			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-			if not xPlayer or not xPlayer.job or not xPlayer.job.name then goto next end
-			local configJob = Config.Jobs[xPlayer.job.name]
-			if not configJob then goto next end
-
-			local allowed = true
-			if configJob.distMin then
-				local coords = xPlayer.getCoords()
-				if #(vector3(coords.x, coords.y, coords.z) - configJob.pos) > configJob.dist then
-					xPlayer.showNotification(Config.Language.NotReceived)
-					goto next
+		-- local xPlayers = ESX.GetPlayers() Bro why you are using this useless ??
+		local players = GetPlayers()
+	
+		-- for i = 1, #xPlayers, 1 do nah bro you're amazing!
+		for playerIndex,playerServerId in pairs(players) do
+		    local xPlayer = ESX.GetPlayerFromId(playerServerId)
+		    local configJob = Config.Jobs[xPlayer.job.name]
+	
+		    if xPlayer and xPlayer.job and xPlayer.job.name and configJob then
+			    -- local allowed = true (imagine declaring a variable without even using it XD)
+			    if configJob.distMin then
+				-- local coords = xPlayer.getCoords()
+				local playerPed      = GetPlayerPed(playerServerId)
+				local playerPosition = GetEntityCoords(playerPed)
+	
+				-- if #(vector3(coords.x, coords.y, coords.z) - configJob.pos) > configJob.dist then (No comment...)
+				if #(playerPosition - configJob.pos) > configJob.dist then
+				    xPlayer.showNotification(Config.Language.NotReceived)
+				else
+				    xPlayer.addAccountMoney("bank", xPlayer.job.grade_salary)
+				    xPlayer.showNotification(Config.Language.Received)
 				end
+			    end
 			end
-			xPlayer.addAccountMoney("bank", xPlayer.job.grade_salary)
-			xPlayer.showNotification(Config.Language.Received)
-			
-			::next::
+		    end
 		end
-		Wait(60 * 1000 * Config.Interval)
+	
+		-- Wait(60 * 1000 * Config.Interval) (wow you really need to vent)
+		Wait(interval)
 	end
 end)
